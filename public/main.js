8d2f7a7714905d3e0ab1144aed52f6af009f6c4d
Danilo Di Cuia <danilo@tezzutezzu.com>
2018-03-04 20:16:06 +0000

    selective interaction with nodes

--- public/main.js
+++ public/main.js
@@ -2,33 +2,30 @@
 const PARTIES = ["M5S", "piùEuropa", "Lega Nord", "Forza Italia", "Potere Al Popolo",  "Fratelli d'Italia", "PD",  "Liberi e Uguali", "Casa Pound"];
 const PARTYCOLORS  = ["#FCDA1B", "#2D9DB4", "#299733", "#13487B", "#DE0016", "#151653", "#EB733F", "#DE0000", "black"];
 
-let width = window.document.body.clientWidth * .7;
-let height = window.document.body.clientHeight;
+let container = d3.select("#container");
+let width = container.node().getBoundingClientRect().width;
+let height = container.node().getBoundingClientRect().height;
 
-let svg = d3.select("body").append("svg").attr("height", height).attr("width", width);
 
+let svg = container.select("svg").attr("height", height);
 let edgesCont = svg.append("g").attr("id", "edgesCont");
 let nodesCont = svg.append("g").attr("id", "nodesCont");
 
 // TOOLTIP
-let tooltip =d3.select("body").append("div").attr("id", "tooltip");
+let tooltip =d3.select("#tooltip");
 
 // SCALES
 let radiusScale = d3.scaleSqrt().clamp(true).range([10,40]);
 let strokeScale = d3.scaleLinear().clamp(true).range([1,5]);
-let distanceScale = d3.scaleLinear().clamp(true).range([width*.2,10]); // lighter weight correspond to higher distances
+let distanceScale = d3.scaleLinear().clamp(true).range([width*.1,10]); // lighter weight correspond to higher distances
 let colorScale = d3.scaleOrdinal().range(PARTYCOLORS).domain(PARTIES);
 
 // DATA
 let graph;
 let selectedSource;
-let selectedTarget;
 
 // PHYSICS
 let simulation;
-let link;
-let node;
-let text;
 
 
 
@@ -42,6 +39,7 @@ function onLoaded(error, data) {
 	data.nodes.forEach(d=>{if(!partiti[d.partito]) partiti[d.partito]= d;});
 
 	graph = data;
+
 	// assign id based on indices
 	graph.nodes.forEach((d,i)=> d.id=i);
 
@@ -62,7 +60,8 @@ function onLoaded(error, data) {
 function updateGraph() {
 
 	let edges = graph.edges.filter(d=>d.weight > 0.1);
-	link = edgesCont.selectAll(".link")
+
+	let link = edgesCont.selectAll(".link")
 	.data(edges, (d)=>d.id); 
 
 	let linkGroups = link
@@ -71,7 +70,7 @@ function updateGraph() {
 	.attr("class", "link")
 	.style("opacity", .7)
 	.on("mouseover", function(d){updateEdgeTooltip(d, d3.select(this));})
-	.on("mouseout", function(){updateEdgeTooltip(null, d3.select(this));});
+	.on("mouseout", function(){updateEdgeTooltip(null, null);});
 
 	linkGroups
 	.append("line")
@@ -84,12 +83,7 @@ function updateGraph() {
 	.attr("class", "realLink")
 	.attr("stroke-width", (d)=>strokeScale(d.weight));
 
-
-	link
-	.exit()
-	.remove();
-
-	node = nodesCont.selectAll(".node")
+	let node = nodesCont.selectAll(".node")
 	.data(graph.nodes, (d)=>d.name);
 
 	node
@@ -112,7 +106,7 @@ function updateGraph() {
 	.remove();
 
 
-	text = svg
+	let text = svg
 	.selectAll(".label")
 	.data(graph.nodes);
 
@@ -143,7 +137,7 @@ function updateGraph() {
 		simulation = d3.forceSimulation()
 		.force("link", d3.forceLink())
 		.force("charge", d3.forceCollide().radius((d)=>radiusScale(d.tweets)*2))
-		.force("center", d3.forceCenter(width / 2, height / 2))
+		.force("center", d3.forceCenter(width * .4, height * .4))
 		.on("tick", ticked);
 	}
 	simulation.nodes(graph.nodes);
@@ -195,14 +189,12 @@ function dragended(d) {
 
 
 
-function updateEdgeTooltip(d, el) {
+function updateEdgeTooltip(edge, el) {
 
 	if(!selectedSource) return;
 
 
-	if(d && d.source.id == selectedSource.id) {
-
-		selectedTarget = d.target;
+	if(edge && (edge.source.id == selectedSource.id || edge.target.id == selectedSource.id)) {
 
 		el
 		.select(".realLink")
@@ -211,7 +203,7 @@ function updateEdgeTooltip(d, el) {
 
 		tooltip
 		.append("h3")
-		.text(`${d.source.name} – ${d.target.name} `);
+		.text(`${edge.source.name} – ${edge.target.name} `);
 
 		tooltip
 		.append("b")
@@ -219,7 +211,7 @@ function updateEdgeTooltip(d, el) {
 
 		tooltip
 		.append("p")
-		.text(d.words.most_similar.map(d=>d[0]).join(", "));
+		.text(edge.words.most_similar.map(d=>d[0]).join(", "));
 
 		tooltip
 		.append("b")
@@ -227,9 +219,9 @@ function updateEdgeTooltip(d, el) {
 
 		tooltip
 		.append("p")
-		.text(d.words.most_different.map(d=>d[0]).join(", "));
+		.text(edge.words.most_different.map(d=>d[0]).join(", "));
 
-		
+		updateNodeStrokes(edge);
 
 
 	} else {
@@ -237,54 +229,68 @@ function updateEdgeTooltip(d, el) {
 		tooltip
 		.html("");
 
-		d3.selectAll(".realLink")
-		.style("stroke", d=> d.source.id == selectedSource.id ? colorScale(d.source.partito) : "#ddd");
+		updateEdgeStrokes();
+		updateNodeStrokes();
 
-		selectedTarget = null;
 
 	}
 	
-	updateNodeStrokes();
 	
 }
 
-function updateNodeStrokes() {
+function updateNodeStrokes(edge) {
 
-	d3.selectAll("circle")
+	nodesCont
+	.selectAll(".node")
 	.style("stroke-width", d=> {
-		let isSelectedSource = selectedSource && d.id == selectedSource.id;
-		let isSelectedTarget = selectedTarget && d.id == selectedTarget.id;
-		return (isSelectedTarget || isSelectedSource)  ? 3 : 0;
+		
+		let selected;
+
+		if(edge) {
+			selected = d.id == edge.source.id || d.id == edge.target.id;
+		} else {
+			selected = d.id == selectedSource.id;
+		}
+
+		return selected ? 3 : 0;
 	});
 
 }
 
+function updateEdgeStrokes() {
+
+	edgesCont
+	.selectAll(".realLink")
+	.style("stroke", d=>{
+		let selected = d.source.id == selectedSource.id || d.target.id == selectedSource.id;
+		return selected ? colorScale(selectedSource.partito) : "#ddd";
+	});
+
+}
 
 function updateSelectedNode(d) {
 
 	d.selected = true;
 	selectedSource = d;
 
-	d3.selectAll(".link")
-	.each(function(d){
+	edgesCont
+	.selectAll(".link")
+	.each(function(edge){
 		
 		let el = d3.select(this);
 
-		el.style("cursor",  edge=> edge.source.id == selectedSource.id ? "pointer" : "default");
-
-		el
-		.style("opacity", edge=> edge.source.id == selectedSource.id ? 1 : .7);
-
-		el	
-		.select(".realLink")
-		.style("stroke", edge=> edge.source.id == selectedSource.id ? colorScale(edge.source.partito) : "#ddd");
-
-		let node = el.node();
-		if( d.source.id == selectedSource.id) {
+		let selected = edge.source.id == selectedSource.id || edge.target.id == selectedSource.id ;
+		
+		el.style("cursor", selected ? "pointer" : "default").style("opacity", selected ? 1 : .7);
+		
+		if( selected) {
+			let node = el.node();
 			node.parentNode.appendChild(node);
 		}
+
 	});
 
+	updateEdgeStrokes(d);
 	updateNodeStrokes();
 
 }
