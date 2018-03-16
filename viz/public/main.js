93e95e6a04c66011b449ee2307063fbd6b525276
Danilo Di Cuia <danilo@tezzutezzu.com>
2018-03-16 11:30:51 +0000

    minor adjustments to graph.

--- viz/public/main.js
+++ viz/public/main.js
@@ -1,6 +1,6 @@
-
-const PARTIES = ["M5S", "piùEuropa", "Lega Nord", "Forza Italia", "Potere Al Popolo",  "Fratelli d'Italia", "PD",  "Liberi e Uguali", "Casa Pound"];
-const PARTYCOLORS  = ["#FCDA1B", "#2D9DB4", "#299733", "#13487B", "#DE0016", "#151653", "#EB733F", "#DE0000", "black"];
+const DEBUG = false;
+const PARTIES = ["M5S", "piùEuropa", "Lega Nord", "Forza Italia", "Potere Al Popolo",  "fratelli", "PD",  "Liberi e Uguali", "Casa Pound"];
+const PARTYCOLORS  = ["#FCDA1B", "#2D9DB4", "#299733", "#13487B", "#DE0016", "#151653", "#EB733F", "#DE0000", "#000000"];
 
 let container = d3.select("#container");
 let width = container.node().getBoundingClientRect().width;
@@ -9,39 +9,100 @@ let height = container.node().getBoundingClientRect().height;
 
 let svg = container.select("svg").attr("height", height);
 let edgesCont = svg.append("g").attr("id", "edgesCont");
-let nodesCont = svg.append("g").attr("id", "nodesCont");
+let nodesCont = d3.select("#nodecontainer");
 
 // TOOLTIP
-let tooltip =d3.select("#tooltip");
+let sidebar =d3.select("#sidebar");
 
 // SCALES
-let radiusScale = d3.scaleSqrt().clamp(true).range([10,40]);
-let strokeScale = d3.scaleLinear().clamp(true).range([1,5]);
-let distanceScale = d3.scaleLinear().clamp(true).range([width*.1,10]); // lighter weight correspond to higher distances
+let radiusScale = d3.scaleSqrt().clamp(true).range([50,80]);
+let strokeScale = d3.scaleLinear().clamp(true).range([2,10]);
+let distanceScale = d3.scaleLinear().clamp(true).range([width*.15,10]); // lighter weight correspond to higher distances
 let colorScale = d3.scaleOrdinal().range(PARTYCOLORS).domain(PARTIES);
+
+// STATE
+const IDLE = 0;
+const NODE_MODE = 1;
+const LINK_MODE = 2;
+let state = IDLE;
+let selectedSource;
+let selectedTarget;
+let selectedEdge;
+
+// GRADIENTS
+let gradients = [];
+PARTIES.forEach(d=>{
+	PARTIES.forEach(dd=>{
+		if(dd!=d) {
+			gradients.push({
+				source:{
+					party:d
+				},
+				target:{
+					party:dd
+				},
+			});
+		}
+	});
+	gradients.push({
+		source:{
+			party:d
+		},
+		target:{
+			party:d
+		},
+	});
+});
+
+
+
+let gradient = svg
+.append("defs")
+.selectAll("linearGradient")
+.data(gradients)
+.enter()
+.append("linearGradient")
+.attr("id", d=>getGradientName(d.source, d.target));
+
+gradient.append("stop")
+.attr("stop-color", d=>colorScale(d.source.party))
+.attr("offset", "0%");
+
+gradient.append("stop")
+.attr("stop-color", d=>colorScale(d.target.party))
+.attr("offset", "100%");
+
 
 // DATA
 let graph;
-let selectedSource;
+let nodes; 
 
 // PHYSICS
 let simulation;
 
 
 
+// START
+d3.json("politicians_graph.json", onLoaded);
+
 
 
 function onLoaded(error, data) {
-
 	if (error) throw error;
 
-	let partiti = {};
-	data.nodes.forEach(d=>{if(!partiti[d.partito]) partiti[d.partito]= d;});
+	let parties = {};
+	data.nodes.forEach(d=>{if(!parties[d.party]) parties[d.party]= d;});
 
 	graph = data;
+	
+	if(DEBUG) {
+		graph.nodes = graph.nodes.slice(0,2);
+		graph.edges = graph.edges.filter(d=>(d.source==0 || d.source==1) && (d.target==0 || d.target==1));
+	}
 
 	// assign id based on indices
 	graph.nodes.forEach((d,i)=> d.id=i);
+	graph.edges = graph.edges.filter(d=>d.weight > 0.1);
 
 	// update scales
 	let sortedWeights = graph.edges.map((d)=>d.weight).sort();
@@ -53,119 +114,99 @@ function onLoaded(error, data) {
 
 
 	updateGraph();
-
 }
 
 
 function updateGraph() {
 
-	let edges = graph.edges.filter(d=>d.weight > 0.1);
+	let edges = graph.edges;
 
-	let link = edgesCont.selectAll(".link")
+	let edge = edgesCont.selectAll(".edge")
 	.data(edges, (d)=>d.id); 
 
-	let linkGroups = link
+	let edgeGroups = edge
 	.enter()
 	.append("g")
-	.attr("class", "link")
-	.style("opacity", .7)
-	.on("mouseover", function(d){updateEdgeTooltip(d, d3.select(this));})
-	.on("mouseout", function(){updateEdgeTooltip(null, null);});
+	.attr("class", "edge")
+	.on("mouseover", function(d){updateEdgeSidebar(d, d3.select(this));})
+	.on("mouseout", function(){updateEdgeSidebar(null, null);});
 
-	linkGroups
-	.append("line")
-	.attr("stroke-width", 10) 
-	.style("stroke-opacity", 0);
+	edgeGroups
+	.append("rect")
+	.attr("width",10)
+	.attr("height",1);
 
 
-	linkGroups
-	.append("line")
-	.attr("class", "realLink")
-	.attr("stroke-width", (d)=>strokeScale(d.weight));
 
-	let node = nodesCont.selectAll(".node")
-	.data(graph.nodes, (d)=>d.name);
-
-	node
+	nodes = nodesCont.selectAll(".node")
+	.data(graph.nodes, (d)=>d.name)
 	.enter()
-	.append("circle")
+	.append("div")
 	.attr("class", "node")
-	.attr("r", (d)=> (radiusScale(d.tweets)))
-	.style("stroke-width", 0)
-	.style("stroke", "black")
-	.style("fill", d=>colorScale(d.partito))
-	.style("fill-opacity", 1)
-	.on("mousedown", updateSelectedNode)
+	// .on("mouseover", onNodeOver)
+	.on("mousedown", onNodeDown)
 	.call(d3.drag()
 		.on("start", dragstarted)
 		.on("drag", dragged)
 		.on("end", dragended));
 
-	node
-	.exit()
-	.remove();
+	nodes
+	.append("div")
+	.attr("class", "nodebg")
+	.style("transform", "translate(-50%,-50%")
+	.style("border-radius", (d)=> (radiusScale(d.tweets)/2)+"px")
+	.style("width", (d)=> radiusScale(d.tweets)+"px")
+	.style("height", (d)=> radiusScale(d.tweets)+"px");
 
-
-	let text = svg
-	.selectAll(".label")
-	.data(graph.nodes);
-
-	text
-	.enter()
-	.append("g")
-	.attr("class", "label")
-	.each(function(d){
-
-		var el = d3.select(this);
-
-		el
-		.append("text")
-		.attr("text-anchor", "middle")
-		.attr("fill", "black")
-		.text(d.name);
-
-	});
-
-
-
-	text
-	.exit()
-	.remove();
+	nodes
+	.append("div")
+	.attr("class", "nodeimage")
+	.style("transform", "translate(-50%,-50%")
+	.style("border", (d)=> `3px solid ${colorScale(d.party)}`)
+	.style("background-image", d=>`url(images/${d.twitter.substr(1)}.jpg)`)
+	.style("border-radius", (d)=> (radiusScale(d.tweets)/2)+"px")
+	.style("width", (d)=> radiusScale(d.tweets)+"px")
+	.style("height", (d)=> radiusScale(d.tweets)+"px");
+	
 
 
 	if(!simulation) {
 		simulation = d3.forceSimulation()
-		.force("link", d3.forceLink())
-		.force("charge", d3.forceCollide().radius((d)=>radiusScale(d.tweets)*2))
-		.force("center", d3.forceCenter(width * .4, height * .4))
+		.force("edge", d3.forceLink())
+		.force("charge", d3.forceCollide().radius((d)=>radiusScale(d.tweets)))
+		.force("center", d3.forceCenter(width * .7, height * .5))
 		.on("tick", ticked);
 	}
+
 	simulation.nodes(graph.nodes);
 
 
-	simulation.force("link")
+	simulation.force("edge")
 	.links(graph.edges)
 	.distance((d)=> {
 		return distanceScale(d.weight) + 200;
 	});
+
+	updateEdgeStrokes();
 }
 
 
-
-
 function ticked() {
-	svg.selectAll(".link").selectAll("line")
-	.attr("x1", (d)=> d.source.x)
-	.attr("y1", (d)=> d.source.y)
-	.attr("x2", (d)=> d.target.x)
-	.attr("y2", (d)=> d.target.y);
+	edgesCont.selectAll(".edge")
+	.attr("transform", d=>`translate(${d.source.x} ${d.source.y})`)
+	.select("rect")
+	.attr("width", d=>{
+		let dx = d.target.x - d.source.x;
+		let dy = d.target.y - d.source.y;
+		return Math.sqrt(dx*dx + dy*dy);
+	})
+	.attr("transform", d=>{
+		let a = Math.atan2(d.target.y-d.source.y, d.target.x - d.source.x) * 180 / Math.PI;
+		return `rotate(${a})`;
+	});
 
-	svg.selectAll(".node")
-	.attr("cx", (d)=> d.x)
-	.attr("cy", (d)=> d.y);
-
-	svg.selectAll(".label")
-	.attr("transform", d=>`translate(${d.x}, ${d.y + radiusScale(d.tweets) + 10})`);
+	nodes
+	.style("transform", d=>`translate(${d.x}px, ${d.y}px)`);
 
 }
 
@@ -187,114 +228,227 @@ function dragended(d) {
 }
 
 
+function onNodeOver(node) {
 
+	if(!selectedSource || node.id == selectedSource.id) return;
+	let edge = findEdgeBetweenNodes(node, selectedSource);
 
-function updateEdgeTooltip(edge, el) {
-
-	if(!selectedSource) return;
-
-
-	if(edge && (edge.source.id == selectedSource.id || edge.target.id == selectedSource.id)) {
-
-		el
-		.select(".realLink")
-		.style("stroke", "black");
-
-
-		tooltip
-		.append("h3")
-		.text(`${edge.source.name} – ${edge.target.name} `);
-
-		tooltip
-		.append("b")
-		.text("Parole in comune più usate");
-
-		tooltip
-		.append("p")
-		.text(edge.words.most_similar.map(d=>d[0]).join(", "));
-
-		tooltip
-		.append("b")
-		.text("Parole in comune meno usate");
-
-		tooltip
-		.append("p")
-		.text(edge.words.most_different.map(d=>d[0]).join(", "));
-
-		updateNodeStrokes(edge);
-
-
-	} else {
-
-		tooltip
-		.html("");
-
-		updateEdgeStrokes();
-		updateNodeStrokes();
-
-
-	}
-	
-	
 }
 
-function updateNodeStrokes(edge) {
+function findEdgeBetweenNodes(nodeA, nodeB) {
 
-	nodesCont
-	.selectAll(".node")
-	.style("stroke-width", d=> {
-		
-		let selected;
-
-		if(edge) {
-			selected = d.id == edge.source.id || d.id == edge.target.id;
-		} else {
-			selected = d.id == selectedSource.id;
+	let edge;
+	graph.edges.forEach(d=>{
+		if((d.source.id == nodeA.id || d.target.id == nodeA.id) && (d.source.id == nodeB.id || d.target.id == nodeB.id)) {
+			edge = d;
 		}
-
-		return selected ? 3 : 0;
 	});
+
+	return edge;
+}
+
+function updateEdgeSidebar(edge, el) {
+}
+
+function updateSidebar() {
+
+	switch(state) {
+		
+		case IDLE:
+		nodes.select(".nodeimage").style("opacity", 1);
+		break;
+
+		case NODE_MODE:
+		let targetNodes = graph.edges.map(d=>{
+			if(d.source==selectedSource) return d.target;
+			if(d.target==selectedSource) return d.source;
+		}).filter(d=>d!=undefined);
+
+		nodes.each(function(d){
+			let el = d3.select(this);
+			let selected = targetNodes.indexOf(d) != -1 || d == selectedSource 
+			el.style("pointer-events", selected ? "auto" : "none");
+			el.select(".nodeimage").style("opacity", selected ? 1 : .5);
+		});
+		break;
+
+		case LINK_MODE:
+		nodes.select(".nodeimage").style("opacity", d=> d == selectedTarget || d == selectedSource ? 1 : .5);
+		break;
+	}
+
+	switch(state) {
+		
+		case IDLE:
+		d3.select("#node-info").style("display", "none");
+		d3.select("#edge-info").style("display", "none");
+		break;
+
+		case NODE_MODE:
+
+		d3.select("#node-info").style("display", "block");
+		d3.select("#node-name").text(selectedSource.name);
+		d3.select("#node-party").text(selectedSource.party);
+		d3.select("#edge-info").style("display", "none");
+
+		break;
+
+		case LINK_MODE:
+
+		d3.select("#node-info").style("display", "none");
+		d3.select("#edge-info").style("display", "block");
+		d3.select("#edge-names").text(`${selectedSource.name} – ${selectedTarget.name}`);
+		
+		
+		// append words in common
+		
+		d3.select("#edge-most-similar")
+		.selectAll("li")
+		.data(selectedEdge.words.most_similar)
+		.enter()
+		.append("li")
+		.text(d=>d[0]);
+
+		d3.select("#edge-most-different")
+		.selectAll("li")
+		.data(selectedEdge.words.most_different)
+		.enter()
+		.append("li")
+		.text(d=>d[0]);
+
+
+		break;
+	}
+
+
+}
+
+function updateNodes() {
+
+	switch(state) {
+		
+		case IDLE:
+		nodes.select(".nodeimage").style("opacity", 1);
+		break;
+
+		case NODE_MODE:
+		let targetNodes = graph.edges.map(d=>{
+			if(d.source==selectedSource) return d.target;
+			if(d.target==selectedSource) return d.source;
+		}).filter(d=>d!=undefined);
+
+		nodes.each(function(d){
+			let el = d3.select(this);
+			let selected = targetNodes.indexOf(d) != -1 || d == selectedSource 
+			el.style("pointer-events", selected ? "auto" : "none");
+			el.select(".nodeimage").style("opacity", selected ? 1 : .1);
+		});
+		break;
+
+		case LINK_MODE:
+		nodes.select(".nodeimage").style("opacity", d=> d == selectedTarget || d == selectedSource ? 1 : .1);
+		break;
+	}
 
 }
 
 function updateEdgeStrokes() {
 
-	edgesCont
-	.selectAll(".realLink")
-	.style("stroke", d=>{
-		let selected = d.source.id == selectedSource.id || d.target.id == selectedSource.id;
-		return selected ? colorScale(selectedSource.partito) : "#ddd";
+	let edges = edgesCont
+	.selectAll(".edge");
+
+	edges
+	.each(function(d){
+		var el = d3.select(this).node();
+		if(d.source == selectedSource || d.target == selectedSource) {
+			el.parentNode.appendChild(el);
+		}
 	});
 
-}
-
-function updateSelectedNode(d) {
-
-	d.selected = true;
-	selectedSource = d;
-
-	edgesCont
-	.selectAll(".link")
-	.each(function(edge){
-		
+	edges
+	.select("rect")
+	.each(function(d){
 		let el = d3.select(this);
 
-		let selected = edge.source.id == selectedSource.id || edge.target.id == selectedSource.id ;
-		
-		el.style("cursor", selected ? "pointer" : "default").style("opacity", selected ? 1 : .7);
-		
-		if( selected) {
-			let node = el.node();
-			node.parentNode.appendChild(node);
+
+		let isActive = d.target == selectedSource || d.source == selectedSource;
+		let gradientName;
+		if(isActive) gradientName = getGradientName(d.source, d.target);			
+
+		switch(state) {
+			case IDLE:
+			el.transition().attr("height", isActive ? strokeScale(d.weight) : 1).attr("fill", "#ddd");
+			break;
+
+			case NODE_MODE:
+			el.attr("fill", gradientName ? `url(#${gradientName})` : "#ddd");
+			el.transition().attr("height", isActive ? strokeScale(d.weight) : 1);
+			break;
+
+			case LINK_MODE:
+			let isLinking = (d.target==selectedTarget || d.source==selectedTarget);
+			el.attr("fill", isLinking  ? `url(#${gradientName})` : "#ddd");
+			el.transition().attr("height", isActive && isLinking ? strokeScale(d.weight) : 1);
+			break;
 		}
 
 	});
 
-	updateEdgeStrokes(d);
-	updateNodeStrokes();
-
 }
 
 
-// START
-d3.json("politicians_graph.json", onLoaded);
+function onNodeDown(d) {
+
+	switch(state) {
+		
+		case IDLE:
+		selectedSource = d;
+		state = NODE_MODE;
+		break;
+
+		case NODE_MODE:
+		if(selectedSource == d) {
+			selectedSource = null;
+			state = IDLE;
+		} else {
+			selectedTarget = d;
+			selectedEdge = findEdgeBetweenNodes(selectedSource, selectedTarget);
+			state = LINK_MODE;
+		}
+		break;
+
+		case LINK_MODE:
+		if(selectedSource == d) {
+			selectedTarget = null;
+			state = NODE_MODE;
+		} else if(selectedTarget == d) {
+			selectedTarget = null;
+			selectedSource = d;
+			state = NODE_MODE;
+		} 
+		break;
+
+	}
+	
+	updateEdgeStrokes();
+	updateNodes();
+	updateSidebar();
+
+}
+
+function getGradientName(source, target) {
+	return `${source.party.replace(/\s/g, "-")}-${target.party.replace(/\s/g, "-")}`;
+}
+
+function addBoxToText() {
+	let bbox = text.node().getBoundingClientRect();
+	let padding = 3;
+
+	el
+	.insert("rect", ":first-child")
+	.attr("x", bbox.x - padding)
+	.attr("y", -bbox.height-padding*.5)
+	.attr("height", bbox.height + padding * 2)
+	.attr("width", bbox.width + padding * 2)
+	.style("fill", "rgba(255,255,255,1)");
+}
